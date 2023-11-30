0466afd53954e4cc430537047bf65e925b17ba92
Kshitij Raj <kshitij.raj@kshitij.raj>
2023-12-01 02:36:05 +0530

    attached event with useRef

--- src/customHook/useNotificationProvider.tsx
+++ src/customHook/useNotificationProvider.tsx
@@ -1,21 +1,21 @@
 import { WindowEvents, WindowEventService } from "@kshitijraj09/sharedlib_mf";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import useNotificationStore from "../zustand-config/notificationStore";
 
 const useNotificationProvider = <Type,>(eventName: WindowEvents) => {
    const { notifications, decreaseNotifications } = useNotificationStore();
-   const [eventType, setEventType] = useState<typeof WindowEventService>();
+   const eventRef = useRef<typeof WindowEventService>();
 
    useEffect(() => {
       import("Sharedlib/eventservice").
          then((event) => {
-            setEventType(event.default);
+            eventRef.current = event.default;
             event.default.subscribe(eventName, updateHandler)
          }).catch(error => {
             console.error('Error occured in event', error);
          })
       return () => {
-         eventType.unsubscribe(eventName, updateHandler)
+         eventRef.current.unsubscribe(eventName, updateHandler)
       }
    }, []);
 
